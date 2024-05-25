508a7209812eadd74766686dc494a3c5720970d2
MadheshKumarVJ <vjmadhesh@testpress.in>
2024-05-25 12:00:59 +0530

    fix: Add models in admin site

--- webby/webhookify/admin.py
+++ webby/webhookify/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Account, Destination
 
-# Register your models here.
+@admin.register(Account)
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ('email', 'account_id', 'name', 'app_secret_token', 'website')
+    search_fields = ('email', 'name','account_id')
+    list_filter = ('name',)
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ('account', 'url', 'http_method', 'headers')
+    search_fields = ('url', 'http_method')
+    list_filter = ('http_method',)
